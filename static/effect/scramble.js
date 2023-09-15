87f16d90bfb9d8c844c7c5f5168c5f3370ed090d
Corvin Schapöhler <scc9le@bosch.com>
2023-09-15 12:08:33 +0200

    Refine scramble effect for easier use and support spaces

--- static/effect/scramble.js
+++ static/effect/scramble.js
@@ -2,16 +2,26 @@ const generateRandomChar = function () {
   return Math.random().toString(36).slice(2, 3); // one character
 };
 
+/**
+ * 
+ * @param {string} targetText 
+ * @param {number} from 
+ * @returns String with scrambled letters attached after target[from]
+ */
 let scramble = function (targetText, from) {
   if (!targetText) return;
   let newText = targetText
     .substring(0, from)
     .padEnd(targetText.length, "_")
     .split("")
-    .map((letter) => {
-      if (letter == "_") {
+    .map((letter, index) => {
+      if (targetText[index] == " ") {
+        return " ";
+      }
+      else if (letter == "_") {
         return generateRandomChar();
-      } else {
+      }
+      else {
         return letter;
       }
     })
@@ -19,12 +29,31 @@ let scramble = function (targetText, from) {
   return newText;
 };
 
+/**
+ * @param {Element} element 
+ * 
+ * @returns
+ */
+const getTextNode = function (element) {
+  if (element.childElementCount == 0) {
+    return element
+  } else {
+    return getTextNode(element.lastElementChild)
+  }
+}
+
+/**
+ * 
+ * @param {Element} element 
+ * @returns 
+ */
 const scrambleElement = function (element) {
   if (!element) return;
   const targetText = element.dataset.value;
+  var textNode = getTextNode(element)
   let iterations = 0;
   const interval = setInterval(() => {
-    element.innerHTML = scramble(targetText, iterations / 3);
+    textNode.innerText = scramble(targetText, iterations / 3);
     if (iterations / 3 >= targetText.length) {
       clearInterval(interval);
     }
@@ -37,5 +66,8 @@ document.querySelectorAll(".scramble").forEach((element) => {
 })
 
 window.onload = () => {
-  document.querySelectorAll(".scramble").forEach(element => scrambleElement(element))
+  document.querySelectorAll(".scramble").forEach((element) => {
+    element.dataset.value = getTextNode(element).innerText
+    scrambleElement(element)
+  })
 }
