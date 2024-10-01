2ede682b55b8260292601ceb8cdae11abafc04bd
Corvin Schapoehler <co.schapoehler@gmail.com>
2024-10-01 17:41:50 +0200

    Fix flickering checkbox

--- static/effect/dark-mode.js
+++ static/effect/dark-mode.js
@@ -1,6 +1,9 @@
 const checkBox = document.getElementById("theme")
 
-checkBox.checked = document.documentElement.dataset.theme === "dark"
+if (document.documentElement.dataset.theme === "dark" && !checkBox.checkBox) {
+    checkBox.checked = true
+}
+
 
 checkBox.addEventListener("change", (a) => {
     if (checkBox.checked) {
--- static/effect/onload-dark-mode.js
+++ static/effect/onload-dark-mode.js
@@ -1,5 +1,11 @@
 let prefersDarkmode = window.matchMedia('(prefers-color-scheme: dark)').matches
 let theme = localStorage.getItem("theme")
 
-document.documentElement.dataset.theme = theme == null ? prefersDarkmode ? "dark" : "light" : theme
-localStorage.setItem(theme, document.documentElement.dataset.theme)
+// theme not set in preferences
+if (theme == null) {
+    document.documentElement.dataset.theme = prefersDarkmode ? "dark" : "light"
+} else {
+    document.documentElement.dataset.theme = theme
+}
+
+localStorage.setItem("theme", document.documentElement.dataset.theme)
